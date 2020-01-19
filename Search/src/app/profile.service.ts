1df040e7ca568a45a16fd0a811c03df4209d461d
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-19 23:38:11 +0300

    added a username in profile.ts where users can input their usernames in their search

--- Search/src/app/profile.service.ts
+++ Search/src/app/profile.service.ts
@@ -10,5 +10,8 @@ export class ProfileService {
   private clientid:'d832d126744c39a0b9b9';
   private clientsecret:'d0993851706e3fe61e39711804c55bd511e7baf7';
 
-  constructor(private http:Http) { }
+  constructor(private http:Http) { 
+  console.log("service is now ready");
+  this.username ='rosa1-maker'  ;
 }
+  }
