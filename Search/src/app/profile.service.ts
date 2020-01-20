cdf2fd8b6f60c721ef028c4fc472ccaf1f610bb8
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-20 14:58:17 +0300

    created a getProfile for repos in profile.service.ts

--- Search/src/app/profile.service.ts
+++ Search/src/app/profile.service.ts
@@ -6,19 +6,19 @@ import { HttpClient } from '@angular/common/http';
 export class ProfileService {
  
   private username:string;
-  private clientid:'d833c093de94beaa4502';
-  private clientsecret:'fb810611732006b108c6e8e5a3c9a5215d42fd61';
+  private clientid:'9a47957648e04b1ef56d';
+  private clientsecret:'9ec1f2fbaa6b928a5315b02ea9365ed9bebe3a1b';
 
   constructor(private http:HttpClient) { 
   console.log("service is now ready");
-  this.username ='rosa1-maker'  ;
+  this.username ='rosa1-maker';
 
 }
 
 getProfileInfo(){
-  return this.http.get("https://api.github.com/users/rosa1-maker" +this.username +"?client_id" +this.username + "&client_secret=" +this.clientsecret);
-
-
-
+  return this.http.get("https://api.github.com/users/" +this.username +"?client_id" +this.username + "&client_secret=" +this.clientsecret);
+}
+getProfileRepos(){
+  return this.http.get("https://api.github.com/users/" +this.username +"/repos?client_id" +this.username + "&client_secret=" +this.clientsecret);
 }
  }
