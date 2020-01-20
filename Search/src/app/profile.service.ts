836c9648d5a8b2121a0066e7d3aa93289eb92d52
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-20 10:53:29 +0300

    added a welcome note in index.html

--- Search/src/app/profile.service.ts
+++ Search/src/app/profile.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,15 +9,15 @@ export class ProfileService {
   private clientid:'d832d126744c39a0b9b9';
   private clientsecret:'d0993851706e3fe61e39711804c55bd511e7baf7';
 
-  constructor(private http:Http) { 
+  constructor(private http:HttpClient) { 
   console.log("service is now ready");
   this.username ='rosa1-maker'  ;
 
 }
 
 getProfileInfo(){
-  return this.http.get("http://api.github.com/users/" +this.username +"?client_id" +this.username + "&client_secret=" +this.clientsecret) 
-  .map (res => res.json());
+  return this.http.get("http://api.github.com/users/" +this.username +"?client_id" +this.username + "&client_secret=" +this.clientsecret);
+
 
 
 }
--- Search/src/app/profile/profile.component.ts
+++ Search/src/app/profile/profile.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
+
 @Component({
-  selector: 'app-profile',
+  selector: 'gg-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  private profile:any[];
+  profile:any[];
 
   constructor(private profileService: ProfileService) {
     this.profileService.getProfileInfo().subscribe(profile =>{
-      console.log(profile)
-      this.profile =profile;
+      console.log(profile);
+      this.profile = profile;
     }) 
    }
 
