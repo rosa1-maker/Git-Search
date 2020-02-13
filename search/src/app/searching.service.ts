14b0ad962e36e7646b918eb7ebecefc68060ed5a
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-13 16:06:53 +0300

    created a searching service where services on searching will be

--- search/src/app/searching.service.ts
+++ search/src/app/searching.service.ts
@@ -1,37 +1,36 @@
-// import { HttpClient, HttpParams } from "@angular/common/http";
-// import { Injectable } from "@angular/core";
-// import { of } from "rxjs";
-// import { map } from "rxjs/operators";
-// import { environment } from "../environments/environment";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
+import { environment } from "../environments/environment";
 
-// const ghUrl = `https://api.github.com/search/users?client_id=${environment.clientId}&client_secret=${environment.clientSecret}`;
-// const PARAMS = new HttpParams({
-//   fromObject: {
-//     client_id: environment.clientId,
-//     client_secret: environment.clientSecret
-//   }
-// });
+const ghUrl = "https://api.github.com/search/users?client_token=${environment.apiKey='d5a6f91f993d023ae3b458ae267e733793d3764e}'";
+const PARAMS = new HttpParams({
+  fromObject: {
+    client_token: environment.apiKey,
+  }
+});
 
-// @Injectable()
-// export class SearchingService {
-//   strArr = [];
-//   constructor(private http: HttpClient) {}
+@Injectable()
+export class SearchingService {
+  strArr = [];
+  constructor(private http: HttpClient) {}
 
-//   search(term: string) {
-//     if (term === "") {
-//       return of([]);
-//     }
+  search(term: string) {
+    if (term === "") {
+      return of([]);
+    }
 
-//     return this.http
-//       .get(ghUrl, { params: PARAMS.set("q", `${term}+in:login`) })
-//       .pipe(
-//         map(response => {
-//           this.strArr = [];
-//           for (const user of response["items"]) {
-//             this.strArr.push(user.login);
-//           }
-//           return this.strArr;
-//         })
-//       );
-//   }
-// }
+    return this.http
+      .get(ghUrl, { params: PARAMS.set("q", `${term}+in:login`) })
+      .pipe(
+        map(response => {
+          this.strArr = [];
+          for (const user of response["items"]) {
+            this.strArr.push(user.login);
+          }
+          return this.strArr;
+        })
+      );
+  }
+}
