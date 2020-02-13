5c51d16ed7bb56b16cbefc902f37fea0c5e402ce
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-13 16:08:17 +0300

    created a user file where user oinformation is kept

---
+++ search/src/app/User.ts
@@ -0,0 +1,11 @@
+export class User {
+  followers: number;
+  avatarUrl: string;
+  bio: string;
+  htmlUrl: string;
+  following: number;
+  login: string;
+  name: string;
+  publicRepos: number;
+    constructor(private username:string,){  }
+}
